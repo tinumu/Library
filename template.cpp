c79996675e634d25c95219fc6affbf40218664a5
tinumu <hirakawa631@i.softbank.jp>
2019-03-13 14:52:05 +0900

    template

--- template.cpp
+++ template.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 using ll = long long;
+using Pi = pair < int, int >;
+
+constexpr int INF = (1 << 30) - 1;
+constexpr int dy[] = { 1, 0, -1, 0, 1, -1, -1, 1 };
+constexpr int dx[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
 
 template < typename T > T gcd(T a, T b) { return ( b ? gcd(b, a % b) : a ); }
 template < typename T > T lcm(T a, T b) { return ( a / gcd(a, b) * b ); }
