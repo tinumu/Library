6fed8f5f8f512abdc8cd1da2d3fd990d02e18f10
tinumu <hirakawa631@i.softbank.jp>
2019-03-13 14:16:42 +0900

    競プロ時に必要なテンプレート追加

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+template < typename T > T gcd(T a, T b) { return ( b ? gcd(b, a % b) : a ); }
+template < typename T > T lcm(T a, T b) { return ( a / gcd(a, b) * b ); }
+
+int main()
+{
+	return ( 0 );
+}
