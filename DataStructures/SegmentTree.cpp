dac0125c51397e6d3c7478960a0f7706d3889af6
tinumu <hirakawa631@i.softbank.jp>
2019-12-05 20:39:24 +0900

    セグ木を久居黄瀬グキにした

--- DataStructures/SegmentTree.cpp
+++ DataStructures/SegmentTree.cpp
@@ -1,79 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = (1 << 30) - 1;
-
-template < typename T >
-struct Monoid {
-	T id;
-	function < T(T, T) > op;
-	Monoid ( T id, function < T(T, T) > op) : id(id), op(op) {}
-};
+const int INF = numeric_limits<int>::max();
 
 template < typename T >
 struct SegmentTree {
-	const Monoid < T > monoid;
+	
+	T id;
+	function < T(T, T) > op;
 	vector < T > dat;
 	int size;
 
-	SegmentTree(int n, Monoid < T > monoid) : monoid(monoid) {
+	SegmentTree(int n, T id, function < T(T, T) > op) : id(id), op(op) {
 		size = 1;
 		while ( size < n ) size *= 2;
-		dat.assign(size * 2 + 10, monoid.id);
+		dat.assign(size * 2 + 10, id);
 	}
 
 	void update(int k, T x) {
-		k += size - 1;
-		dat[k] = x;
-		while ( k > 0 ) {
-			k = (k - 1) / 2;
-			dat[k] = monoid.op(dat[k * 2 + 1], dat[k * 2 + 2]);
-		}
+		k += size, dat[k] = x;
+		while ( k > 1 ) k >>= 1, dat[k] = op(dat[k << 1], dat[(k << 1) | 1]);
 	}
-	
-	void add(int k, T x) { update(k, monoid.op(x, dat[k + size - 1])); }
-	T query(int a, int b) { return (query(a, b, 0, 0, size - 1)); }
 
-	T query(int a, int b, int k, int l, int r) {
-		T vl, vr;
-		if ( r < a || b < l ) return ( monoid.id );
-		if ( a <= l && r <= b ) return ( dat[k] );
-		vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-		vr = query(a, b, k * 2 + 2, (l + r) / 2 + 1, r);
-		return ( monoid.op(vl, vr) );
+	void merge(int k, T x) { update(k, op(x, dat[k + size])); }
+	//[a, b)
+	T query(int a, int b) {
+		T tl = id, tr = id;
+		for ( int l = a + size, r = b + size; l < r; l >>= 1, r >>= 1 ) {
+			if ( l & 1 ) tl = op(tl, dat[l++]);
+			if ( r & 1 ) tr = op(tr, dat[--r]);
+		}
+		return ( op(tl, tr) );
 	}
 };
 
 int main()
 {
-	int N;
-	string S;
-
-	cin >> N;
-
+	int N, Q;
+	cin >> N >> Q;
 	//RMQ
-	SegmentTree < int > seg(N, Monoid < int > (INF, [](int a, int b) { return (min(a, b)); } ));
-	
-	for ( int i = 0; i < N; i++ ) {
-		int val;
-		cin >> val;
-		seg.update(i, val);
+	SegmentTree < int > seg(N, INF, [](int a, int b) { return (min(a, b)); } );
+
+	while ( Q-- ) {
+		int com, a, b;
+		cin >> com >> a >> b;
+		if ( com == 0 ) seg.update(a, b);
+		else cout << seg.query(a, b + 1) << endl;
 	}
-	
-	while ( cin >> S, S != "exit" ) {
-		switch ( S[0] ) {
-		case 'm' : //min
-			int a, b;
-			cin >> a >> b;
-			cout << seg.query(a, b) << endl;
-			break;
-		case 'u' : //update
-			int k, x;
-			cin >> k >> x;
-			seg.update(k, x);
-			break;
-		}
-	}
-	
+		
 	return ( 0 );
 }
