a289d1ed7733408a8062252debdf42054ca039a1
tinumu <hirakawa631@i.softbank.jp>
2019-12-17 22:53:07 +0900

    template

--- template.cpp
+++ template.cpp
@@ -12,8 +12,8 @@ constexpr int INF = (1 << 30) - 1;
 constexpr ll LLINF = 1LL << 60;
 constexpr int dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
 constexpr int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+constexpr char el = '\n';
 
-template<typename T> T gcd(T a, T b) { return (b ? gcd(b, a % b) : a); }
 template<typename T> T lcm(T a, T b) { return (a / gcd(a, b) * b); }
 
 int main() {
