d69e003b05c3c14377cab18e6b85f96eded90920
tinumu <hirakawa631@i.softbank.jp>
2019-12-17 20:38:55 +0900

    foo

--- template.cpp
+++ template.cpp
@@ -5,16 +5,17 @@ using ll = long long;
 using Pi = pair<int, int>;
 using Pl = pair<ll, ll>;
 using vint = vector<int>;
+template<typename T> using uset = unordered_set<T>;
+template<typename T1, typename T2> using umap = unordered_map<T1, T2>;
 
 constexpr int INF = (1 << 30) - 1;
 constexpr ll LLINF = 1LL << 60;
-constexpr int dy[] = { 1, 0, -1, 0, 1, -1, -1, 1 };
-constexpr int dx[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
+constexpr int dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+constexpr int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
 
-template < typename T > T gcd(T a, T b) { return ( b ? gcd(b, a % b) : a ); }
-template < typename T > T lcm(T a, T b) { return ( a / gcd(a, b) * b ); }
+template<typename T> T gcd(T a, T b) { return (b ? gcd(b, a % b) : a); }
+template<typename T> T lcm(T a, T b) { return (a / gcd(a, b) * b); }
 
 int main() {
-	return (0);
+    return (0);
 }
-
