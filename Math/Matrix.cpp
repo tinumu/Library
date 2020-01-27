1238a0fd2408d65dac06ddfce28ec41247ed3c58
tinumu <hirakawa631@i.softbank.jp>
2020-01-27 18:02:51 +0900

    Matrix

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -1,131 +1,123 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template < typename T > 
-struct Matrix
-{
-	using V = vector < T >;
-	using VV = vector < V >;
-	VV mat;
+constexpr char el = '\n';
 
-	Matrix ( int n )
-	{
+template<typename T> 
+struct Matrix {
+	using V = vector<T>;
+	using VV = vector<V>;
+	VV mat;
+     
+	Matrix (int n) {
 		mat.assign(n, V(n, 0));
 	}
 	
-	void input()
-	{
-		int n = mat.size();
-		for ( int i = 0; i < n; i++ ) {
-			for ( int j = 0; j < n; j++ ) {
-				cin >> mat[i][j];
+	friend istream& operator >>(istream& is, Matrix &d) {
+		int n = d.mat.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				is >> d.mat[i][j];
 			}
 		}
+        return (is);
 	}
 
-	void output()
-	{
-		int n = mat.size();
-		for ( int i = 0; i < n; i++ ) {
-			cout << mat[i][0];
-			for ( int j = 1; j < n; j++ ) {
-				cout << " " << mat[i][j];
+	friend ostream& operator <<(ostream& os, Matrix &d) {
+		int n = d.mat.size();
+		for (int i = 0; i < n; i++) {
+			os << d.mat[i][0];
+			for (int j = 1; j < n; j++) {
+				os << " " << d.mat[i][j];
 			}
-			cout << endl;
+			os << el;
 		}
+        return (os);
 	}
 
-	Matrix operator + ( const Matrix &d ) const
-	{
+	Matrix operator + ( const Matrix &d ) const {
 		int n = mat.size();
 		Matrix ret(n);
-		if ( n == d.mat.size() ) {
-			for ( int i = 0; i < n; i++ ) {
-				for ( int j = 0; j < n; j++ ) {
+		if (n == d.mat.size()) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
 					ret.mat[i][j] = mat[i][j] + d.mat[i][j];
 				}
 			}
 		}
-		return ( ret );
+		return (ret);
 	}
 
-	Matrix operator - ( const Matrix &d ) const
-	{
+	Matrix operator - (const Matrix &d) const {
 		int n = mat.size();
 		Matrix ret(n);
-		if ( n == d.mat.size() ) {
-			for ( int i = 0; i < n; i++ ) {
-				for ( int j = 0; j < n; j++ ) {
+		if (n == d.mat.size()) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
 					ret.mat[i][j] = mat[i][j] - d.mat[i][j];
 				}
 			}
 		}
-		return ( ret );
+		return (ret);
 	}
 	
-	Matrix operator * ( const Matrix &d ) const
-	{
+	Matrix operator * (const Matrix &d) const {
 		int n = mat.size();
 		Matrix ret(n);
-		if ( n == d.mat.size() ) {
-			for ( int i = 0; i < n; i++ ) {
-				for ( int j = 0; j < n; j++ ) {
-					for ( int k = 0; k < n; k++ ) {
+		if (n == d.mat.size()) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					for (int k = 0; k < n; k++) {
 						ret.mat[i][j] += mat[i][k] * d.mat[k][j];
 					}
 				}
 			}
 		}
-		return ( ret );
+		return (ret);
 	}
 	
-	V operator * ( const V &d ) const
-	{
+	V operator * (const V &d) const {
 		int n = mat.size();
 		V ret(n);
-		if ( n == d.size() ) {
-			for ( int i = 0; i < n; i++ ) {
-				for ( int k = 0; k < n; k++ ) {
+		if (n == d.size()) {
+			for (int i = 0; i < n; i++) {
+				for (int k = 0; k < n; k++) {
 					ret[i] += mat[i][k] * d[k];
 				}
 			}
 		}
-		return ( ret );
+		return (ret);
 	}
 
-	Matrix operator ^ ( int w ) const
-	{
+	Matrix operator ^ ( int w ) const {
 		Matrix ret = I(), pad = *this;
 		
-		while ( w ) {
-			if ( w & 1 ) ret = ret * pad;
+		while (w) {
+			if (w & 1) ret = ret * pad;
 			pad = pad * pad;
 			w >>= 1;
 		}
 
-		return ( ret );
+		return (ret);
 	}
 
-	Matrix I() const
-	{
+	Matrix I() const {
 		int n = mat.size();
 		Matrix ret(n);
-		for ( int i = 0; i < n; i++ ) {
+		for (int i = 0; i < n; i++) {
 			ret.mat[i][i] = 1;
 		}
-		return ( ret );
+		return (ret);
 	}
 };
 
-int main()
-{
-	Matrix < int > a(3), b(3), ans(3);
-	
-	a.input();
+int main() {
+	Matrix<int> a(3), b(3), ans(3);
+    cin >> a;
 
 	ans = a^4;
 
-	ans.output();
+    cout << ans;
 
-	return ( 0 );
+	return (0);
 }
