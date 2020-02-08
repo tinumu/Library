5859e14bdc86910fb63d2674b263757e39b47030
tinumu <hirakawa631@i.softbank.jp>
2020-02-08 12:02:43 +0900

    BellmanFord syntax modified

--- Graph/BellmanFord.cpp
+++ Graph/BellmanFord.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template < typename T >
+template<typename T>
 struct Edge {
     int from, to;
     T cost;
@@ -10,20 +10,20 @@ struct Edge {
 	Edge (int to, T cost) : from(-1), to(to), cost(cost) {}
 };
 
-template < typename T >
-vector < T > BellmanFord(int V, vector < Edge < T > > &edges, int s)
+template<typename T>
+vector<T> BellmanFord(int V, vector<Edge<T>> &edges, int s)
 {
-    constexpr T INF = numeric_limits < T >::max();
-    vector < T > d(V, INF);
+    constexpr T INF = numeric_limits<T>::max();
+    vector<T> d(V, INF);
     d[s] = 0;
-    for ( int i = 0; i < V; i++ ) {
-        for ( auto &e : edges ) {
-            if ( d[e.from] != INF && d[e.to] > d[e.from] + e.cost ) {
+    for (int i = 0; i < V; i++) {
+        for (auto &e : edges) {
+            if (d[e.from] != INF && d[e.to] > d[e.from] + e.cost) {
                 d[e.to] = d[e.from] + e.cost;
-                if ( i == V - 1 ) return ( vector < T > () );
+                if (i == V - 1) return (vector<T>());
             }
         }
     }
-    return ( d );
+    return (d);
 }
 
