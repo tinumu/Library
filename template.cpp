2ccb036ebf0e286059ddc019d9cd7de1d360841d
tinumu <hirakawa631@i.softbank.jp>
2020-02-12 18:53:47 +0900

    pair template

--- template.cpp
+++ template.cpp
@@ -38,8 +38,7 @@ istream& operator >>(istream &is, vector<T> &v) {
 
 template<typename T1, typename T2>
 istream& operator >>(istream &is, pair<T1, T2> &p) {
-	is >> p.first >> p.second;
-	return (is);
+	return (is >> p.first >> p.second);
 }
 
 int main() {
