4afd75fb451332218a2ea742553df9fe1137c0df
tinumu <intothesky@yahoo.ne.jp>
2023-12-29 23:05:14 +0900

    a

--- Math/extgcd.cpp
+++ Math/extgcd.cpp
@@ -14,6 +14,22 @@ T extgcd(T a, T b, T &x, T &y) {
 	return (d);
 }
 
+//ansx は動くことは確かめられた
+//x が非負整数で一番小さい値になるように調整してある
+template<typename T>
+tuple<T, T, bool> extgcd(T a, T b, T c) {
+	T x, y;
+	T g = gcd(a, b);
+	if ((g + c%g)%g != 0) return (tuple<T, T, bool>(0, 0, false));
+	extgcd(a, b, x, y);
+	T ansx = x*c/g, ansy = y*c/g;
+	auto bdiv = [](T v, T m) { return (v < 0 ? (v-m+1)/m : v/m); };
+	T t = bdiv(ansx, b/g);
+	ansx -= t*(b/g);
+	ansy -= t*(a/g);
+	return (tuple<T, T, bool>(ansx, ansy, true));
+}
+
 //modInv はまだ確かめてない
 template<typename T>
 T modInv(T dat, T mod) {
